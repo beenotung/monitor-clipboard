0df6ba679cffd0d7eb4317d8208e7afb5e483341
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-18 19:16:55 +0800

    feat: make the core functions customizable feat: take cli arguments (optional) docs: add help message

---
+++ src/cli.ts
@@ -0,0 +1,177 @@
+import { existsSync, mkdirSync, writeFileSync } from 'fs'
+import {
+  addToIgnore,
+  no_trim,
+  restoreFromLastFile,
+  toFilePath,
+  trim,
+  watchClipboard,
+} from './core'
+
+function showHelp() {
+  console.log(
+    `
+Usage: clipboard-monitor [options]
+
+Examples:
+  clipboard-monitor --dir chats
+
+Options (all optional):
+  --help, -h          Show this help message (and then exit)
+  --ignore-file       File to store ignore patterns          default: ./.gitignore
+                      e.g. ../.gitignore
+  --dir               Directory to store the files           default: ./chats
+                      e.g. ~/logs/chats/
+  --prefix            Prefix of the file name                default: (empty)
+                      e.g. message_
+  --suffix            Suffix of the file name                default: (empty)
+                      e.g. _clipboard
+  --ext               Extension of the file name             default: txt
+                      e.g. txt, md, csv, tsv, json, etc
+  --interval          Interval to check clipboard, ms        default: 100
+                      e.g. 300, 500, 800
+  --auto-trim         Automatically trim the content         default: yes
+  --no-trim           Do not trim the content
+`.trim(),
+  )
+  process.exit(0)
+}
+
+function getArgs() {
+  let ignore_file = '.gitignore'
+  let dir = 'chats'
+  let prefix = ''
+  let suffix = ''
+  let ext = 'txt'
+  let interval = 100
+  let auto_trim = true
+
+  let args = process.argv.slice(2)
+  for (let i = 0; i < args.length; i++) {
+    let arg = args[i]
+    if (arg === '--help' || arg === '-h') {
+      showHelp()
+      process.exit(0)
+    }
+    if (arg === '--ignore-file') {
+      ignore_file = args[i + 1]
+      i++
+      continue
+    }
+    if (arg === '--dir') {
+      dir = args[i + 1]
+      i++
+      continue
+    }
+    if (arg === '--prefix') {
+      prefix = args[i + 1]
+      i++
+      continue
+    }
+    if (arg === '--suffix') {
+      suffix = args[i + 1]
+      i++
+      continue
+    }
+    if (arg === '--ext') {
+      ext = args[i + 1]
+      i++
+      continue
+    }
+    if (arg === '--interval') {
+      interval = +args[i + 1]
+      i++
+      continue
+    }
+    if (arg === '--auto-trim') {
+      auto_trim = true
+      continue
+    }
+    if (arg === '--no-trim') {
+      auto_trim = false
+      continue
+    }
+    console.error(`unknown argument: ${JSON.stringify(arg)}`)
+    let selfExec = getSelfExec()
+    console.error(`Run \`${selfExec} --help\` for help.`)
+    process.exit(1)
+  }
+  return { ignore_file, dir, prefix, suffix, ext, interval, auto_trim }
+}
+
+function isWithNpx() {
+  return (
+    process.env.npm_config_argv?.includes('npx') ||
+    process.env.npm_lifecycle_event?.includes('npx') ||
+    false
+  )
+}
+
+function getSelfExec() {
+  if (isWithNpx()) {
+    return 'npx clipboard-monitor'
+  }
+  return 'clipboard-monitor'
+}
+
+async function main() {
+  let args = getArgs()
+  let { ignore_file, dir, prefix, suffix, ext, interval, auto_trim } = args
+
+  let normalize = auto_trim ? trim : no_trim
+
+  addToIgnore({ file: ignore_file, pattern: dir })
+
+  mkdirSync(dir, { recursive: true })
+
+  let lastFile = restoreFromLastFile({ dir, normalize, prefix, suffix, ext })
+  let seq = lastFile.seq + 1
+  let last_content = normalize(lastFile.content)
+
+  if (lastFile.file) {
+    log(`last save: ${lastFile.file}: ${previewContent(last_content)}`)
+  }
+
+  let stream = watchClipboard({
+    last_content,
+    interval,
+    normalize,
+  })
+  for await (let content of stream) {
+    for (;;) {
+      let file = toFilePath({ dir, seq, prefix, suffix, ext, content })
+      if (existsSync(file)) {
+        seq++
+        continue
+      }
+      writeFileSync(file, content)
+      log(`new save: ${file}: ${previewContent(content)}`)
+      break
+    }
+  }
+}
+
+function previewContent(content: string) {
+  let head = content.slice(0, 20)
+  let tail = content.slice(-20)
+  return `\n${head}\n...\n${tail}\n`
+}
+
+function log(message: string) {
+  let timestamp = getTimestamp()
+  console.log(`[${timestamp}] ${message}`)
+}
+
+function getTimestamp() {
+  let date = new Date()
+  let h = date.getHours().toString().padStart(2, '0')
+  let m = date.getMinutes().toString().padStart(2, '0')
+  let s = date.getSeconds().toString().padStart(2, '0')
+  let ms = date.getMilliseconds().toString().padStart(3, '0')
+  return `${h}:${m}:${s}.${ms}`
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
---
+++ src/core.ts
@@ -0,0 +1,145 @@
+import { execSync } from 'child_process'
+import { existsSync, readdirSync, readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
+
+export function readClipboard() {
+  return execSync('xclip -sel clipboard -o').toString()
+}
+
+export async function* watchClipboard(options: {
+  last_content: string
+  interval: number
+  normalize: (content: string) => string
+}) {
+  let normalize = options.normalize
+  let last_content = normalize(options.last_content)
+  for (;;) {
+    let content = normalize(readClipboard())
+    if (content === last_content) {
+      await sleep(options.interval)
+      continue
+    }
+    yield content
+    last_content = content
+    await sleep(options.interval / 2)
+  }
+}
+
+export function trim(content: string) {
+  return content.trim()
+}
+
+export function no_trim(content: string) {
+  return content
+}
+
+export function getLastFile(options: {
+  dir: string
+  prefix: string
+  suffix: string
+  ext: string
+}): { seq: number; filename: string } | null {
+  let { dir, prefix, suffix, ext } = options
+  let filenames = readdirSync(dir)
+  let files = parseFilenames({ prefix, suffix, ext, filenames })
+  let last = files
+    .filter(file => file !== null)
+    .sort((a, b) => b.seq - a.seq)[0]
+  return last || null
+}
+
+export function parseFilenames(options: {
+  prefix: string
+  suffix: string
+  ext: string
+  filenames: string[]
+}) {
+  let { prefix, suffix, ext, filenames } = options
+
+  if (ext.startsWith('.')) {
+    ext = ext.slice(1)
+  }
+  let after = suffix + '.' + ext
+  let before = prefix
+
+  return filenames.map(filename => {
+    if (filename.startsWith(before) && filename.endsWith(after)) {
+      let seq = +filename.slice(before.length, -after.length)
+      return { seq, filename }
+    }
+    return null
+  })
+}
+
+export function restoreFromLastFile(options: {
+  dir: string
+  normalize: (content: string) => string
+  prefix: string
+  suffix: string
+  ext: string
+}) {
+  let { dir, normalize, prefix, suffix, ext } = options
+  let last_file = getLastFile({ dir, prefix, suffix, ext })
+  if (!last_file) {
+    return {
+      seq: 0,
+      file: '',
+      content: '',
+    }
+  }
+  let file = join(dir, last_file.filename)
+  let content = normalize(readFileSync(file).toString())
+  return {
+    seq: last_file.seq,
+    file,
+    content,
+  }
+}
+
+export function toFilePath(options: {
+  dir: string
+  seq: number
+  prefix: string
+  suffix: string
+  content: string
+  /** default is '.txt' */
+  ext: string
+}) {
+  let ext = options.ext
+  if (ext.startsWith('.')) {
+    ext = ext.slice(1)
+  }
+
+  let filename = [
+    options.prefix || '',
+    options.seq,
+    options.suffix || '',
+    '.',
+    ext,
+  ].join('')
+  let file = join(options.dir, filename)
+
+  return file
+}
+
+export function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export function addToIgnore(options: { file: string; pattern: string }) {
+  let { file, pattern } = options
+
+  if (pattern.endsWith('/')) {
+    pattern = pattern.slice(0, -1)
+  }
+
+  let lines = existsSync(file)
+    ? readFileSync(file).toString().trim().split('\n')
+    : []
+  if (lines.includes(pattern) || lines.includes(pattern + '/')) {
+    return
+  }
+  lines.push(pattern + '/')
+  let content = lines.join('\n') + '\n'
+  writeFileSync(file, content)
+}
